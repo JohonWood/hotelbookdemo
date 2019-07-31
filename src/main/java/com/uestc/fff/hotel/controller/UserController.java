70a999784756a0f9cf3c724fa90fd0b84817af19
吴俊宏 <814411678@qq.com>
2019-07-31 14:06:12 +0800

    7.31 14:06

--- src/main/java/com/uestc/fff/hotel/controller/UserController.java
+++ src/main/java/com/uestc/fff/hotel/controller/UserController.java
@@ -37,13 +37,6 @@ public class UserController {
          response.setContentType("text/html;charset=utf-8");//操作返回消息提示
          try (PrintWriter writer = response.getWriter())
          {
-             if (userID == null || userName == null || userPhone == null || loginName == null || loginPassword == null)
-             //判断输入的注册信息是否有空值
-             {
-                 writer.write("<script> alert('请将资料填写完整');history.go(-1); </script>");
-                 //System.out.println("请将资料填写完整");
-                 //return "register";
-             }
              UserInfo testid = userService.findUserByUserID(userID);
              if (testid == null) {
                  UserInfo dbUser = new UserInfo();
@@ -55,12 +48,8 @@ public class UserController {
                  userService.insertUserInfo(dbUser);
                  //保存注册信息到数据库
                  writer.write("<script> alert('注册成功,请登录'); location.href='login';</script>");
-                 //System.out.println("注册成功");
-                 //return "login";
              } else {
                  writer.write("<script> alert('账号已被使用，请重新注册'); history.go(-1);</script>");
-                 //System.out.println("帐号" + userID + "已被使用,请重新注册");
-                 //return "register";
              }
              writer.flush();
          } catch (IOException e) {
@@ -87,28 +76,21 @@ public class UserController {
             if(dbUser == null)
             {
                 writer.write("<script> alert('该账号不存在'); history.go(-1);</script>");
-                //System.out.println("该用户不存在");
-                //return "login";
+
             }
             else if( !dbUser.getLoginPassword().equals(userPassword) )
             {
                 writer.write("<script> alert('密码错误'); history.go(-1);</script>");
-                //System.out.println("密码错误");
-                //return "login";
+
             }
             else
             {
-
                 //用session保存用户登录信息
                 session.setAttribute("user", dbUser);
-
-
-
                 //创建cookie对象来保存session的id
-                Cookie cookie = new Cookie("jessionid",session.getId());
+                Cookie cookie = new Cookie("JSESSIONID",session.getId());
                 cookie.setMaxAge(86400);//保存一天
                 response.addCookie(cookie);
-
                 System.out.println(dbUser.getUserId());
                 if ( "admin".equals(dbUser.getUserId())){
                     writer.write("<script> alert('登录成功'); location.href='/manage/country';</script>");
@@ -116,9 +98,6 @@ public class UserController {
                 else{
                     writer.write("<script> alert('登录成功'); location.href='/504/host?islogin=true';</script>");
                 }
-
-                //System.out.println("登录成功");
-                //return "testlogin";
             }
         } catch (IOException e) {
             e.printStackTrace();
