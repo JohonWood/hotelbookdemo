93f93ce54dfb31b9d882c272f1b5b10a158460a1
吴俊宏 <814411678@qq.com>
2019-08-01 18:44:59 +0800

    "8.1 18:44"

--- src/main/java/com/uestc/fff/hotel/controller/UserController.java
+++ src/main/java/com/uestc/fff/hotel/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.uestc.fff.hotel.controller;
 
 import com.uestc.fff.hotel.domain.UserInfo;
+import com.uestc.fff.hotel.domain.order;
 import com.uestc.fff.hotel.service.UserService;
+import com.uestc.fff.hotel.service.searchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
-
+import java.util.List;
 
 
 @Controller
@@ -21,6 +23,8 @@ import java.io.PrintWriter;
 public class UserController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private searchService serviceSearch;
 
     /************注册****************/
     @RequestMapping(value = "/register")
@@ -82,11 +86,17 @@ public class UserController {
 
         response.setContentType("text/html;charset=utf-8");//操作返回消息提示
         try (PrintWriter writer = response.getWriter()) {
+            if("".equals(userID) ||  "".equals(userPassword))
+            {
+                writer.write("<script> alert('请将帐号密码填写完整'); history.go(-1);</script>");
+                writer.flush();
+                return;
+            }
             UserInfo dbUser = userService.findUserByUserID(userID);
             if (dbUser == null) {
                 writer.write("<script> alert('该账号不存在'); history.go(-1);</script>");
                 writer.flush();
-                
+
 
             } else if (!dbUser.getLoginPassword().equals(userPassword)) {
                 writer.write("<script> alert('密码错误'); history.go(-1);</script>");
@@ -136,9 +146,93 @@ public class UserController {
             }
             //从session中删除user
             session.removeAttribute("user");
-            writer.write("<script> alert('成功登出'); location.href='/504/host';</script>");
+            writer.write("<script> alert('成功登出'); location.href='/504/host';</script>");//登出后返回主页面
             writer.flush();
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @GetMapping("/userinfo")
+    public String userInfoPages(Model model, HttpSession session){
+        boolean islogin;
+        UserInfo userInfotest = (UserInfo) session.getAttribute("user");
+        if (userInfotest == null) { islogin = false; }
+        else  { islogin = true; }
+        model.addAttribute("isLogin",islogin);
+        model.addAttribute("User_name",userInfotest.getLoginName());
+        model.addAttribute("numOfOrders",serviceSearch.countOrder("1"));
+        List<order> orderList=serviceSearch.orderList("1");
+        model.addAttribute("orderList",orderList);
+
+        model.addAttribute("userID",userInfotest.getUserId());
+        model.addAttribute("userName",userInfotest.getUserName());
+        model.addAttribute("userPhone",userInfotest.getUserPhone());
+        model.addAttribute("loginName",userInfotest.getLoginName());
+        return "UserInfo";
+    }
+
+    @PostMapping("/userinfo")
+    public void changeUserInfo(HttpSession session, HttpServletResponse response,
+                               @RequestParam("name") String userName,
+                               @RequestParam("phone") String userPhone,
+                               @RequestParam("loginname") String loginName)
+    {
+        response.setContentType("text/html;charset=utf-8");
+        UserInfo changeUser = (UserInfo) session.getAttribute("user");
+        changeUser.setUserName(userName);
+        changeUser.setUserPhone(userPhone);
+        changeUser.setLoginName(loginName);
+        userService.updateUserInfo(changeUser);
+        try {
+            response.getWriter().write("<script>alert('成功更新用户信息');location.href='/user/userinfo';</script>");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @GetMapping("/changepassword")
+    public String changePasswordPage(Model model, HttpSession session)
+    {
+        boolean islogin;
+        UserInfo userInfotest = (UserInfo) session.getAttribute("user");
+        if (userInfotest == null) { islogin = false; }
+        else  { islogin = true; }
+        model.addAttribute("isLogin",islogin);
+        model.addAttribute("User_name",userInfotest.getLoginName());
+        model.addAttribute("numOfOrders",serviceSearch.countOrder("1"));
+        List<order> orderList=serviceSearch.orderList("1");
+        model.addAttribute("orderList",orderList);
+        return "ChangePassword";
+    }
+
+    @PostMapping("/changepassword")
+    public void changePassword(HttpSession session, HttpServletResponse response,
+                               @RequestParam("originpassword") String originPassword,
+                               @RequestParam("newpassword") String newPassword,
+                               @RequestParam("newpassword2") String newPassword2)
+    {
+        response.setContentType("text/html;charset=utf-8");
+        UserInfo changeUser = (UserInfo) session.getAttribute("user");
+        try
+        {
+            if(!changeUser.getLoginPassword().equals(originPassword))
+            {
+                response.getWriter().write("<script> alert('原密码不正确'); history.go(-1);</script>");
+            }
+            if (!newPassword.equals(newPassword2))
+            {
+                response.getWriter().write("<script> alert('两次新密码输入不一致'); history.go(-1);</script>");
+            }
+            else
+            {
+                changeUser.setLoginPassword(newPassword);
+                userService.updateUserInfo(changeUser);
+                response.getWriter().write("<script> alert('密码修改成功'); location.href='/user/changepassword';</script>");
+            }
+        }
+        catch (IOException e)
+        {
             e.printStackTrace();
         }
     }
