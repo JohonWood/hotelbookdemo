7597332621369070915e258ec114ac0dab8fa9b3
吴俊宏 <814411678@qq.com>
2019-08-07 09:55:16 +0800

    添加了数据库密码加密功能

--- src/main/java/com/uestc/fff/hotel/controller/UserController.java
+++ src/main/java/com/uestc/fff/hotel/controller/UserController.java
@@ -9,12 +9,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.security.SecureRandom;
+import java.util.Base64;
+import java.util.Base64.Encoder;
+import java.util.Base64.Decoder;
 import java.util.List;
 
 
@@ -38,30 +46,25 @@ public class UserController {
                               @RequestParam("userPhone") String userPhone,
                               @RequestParam("loginName") String loginName,
                               @RequestParam("loginPassword") String loginPassword,
+                              @RequestParam("loginPasswordAgain") String loginPasswordAgain,
                               HttpServletResponse response) {
         response.setContentType("text/html;charset=utf-8");//操作返回消息提示
         try (PrintWriter writer = response.getWriter()) {
-            if("".equals(userID) || "".equals(userName) || "".equals(userPhone) || "".equals(loginName) || "".equals(loginPassword))
-            {
-                writer.write("<script> alert('请填写完整信息'); history.go(-1);</script>");
-                writer.flush();
-                return;
-            }
             UserInfo testid = userService.findUserByUserID(userID);
-            if (testid == null) {
+            if (testid == null && loginPassword.equals(loginPasswordAgain)) {
                 UserInfo dbUser = new UserInfo();
                 dbUser.setUserId(userID);
                 dbUser.setUserName(userName);
                 dbUser.setUserPhone(userPhone);
                 dbUser.setLoginName(loginName);
-                dbUser.setLoginPassword(loginPassword);
+                dbUser.setLoginPassword(encryptBasedDes(loginPassword));//将加密后的密码存入
                 userService.insertUserInfo(dbUser);
                 //保存注册信息到数据库
                 writer.write("<script> alert('注册成功,请登录'); location.href='login';</script>");
                 writer.flush();
                 
             } else {
-                writer.write("<script> alert('账号已被使用，请重新注册'); history.go(-1);</script>");
+                writer.write("<script> alert('账号已被使用或两次输入密码不一致，请重新注册'); history.go(-1);</script>");
                 writer.flush();
                 
             }
@@ -86,22 +89,21 @@ public class UserController {
 
         response.setContentType("text/html;charset=utf-8");//操作返回消息提示
         try (PrintWriter writer = response.getWriter()) {
-            if("".equals(userID) ||  "".equals(userPassword))
-            {
-                writer.write("<script> alert('请将帐号密码填写完整'); history.go(-1);</script>");
-                writer.flush();
-                return;
-            }
+
             UserInfo dbUser = userService.findUserByUserID(userID);
-            if (dbUser == null) {
+
+            if (dbUser == null)
+            {
                 writer.write("<script> alert('该账号不存在'); history.go(-1);</script>");
                 writer.flush();
+                return;
 
-
-            } else if (!dbUser.getLoginPassword().equals(userPassword)) {
+            }
+            if (!userPassword.equals(decryptBasedDes(dbUser.getLoginPassword())))//将数据库中加密的密码解密后与输入的密码进行比较
+             {
                 writer.write("<script> alert('密码错误'); history.go(-1);</script>");
                 writer.flush();
-                
+                return;
 
             } else {
                 //用session保存用户登录信息
@@ -194,7 +196,7 @@ public class UserController {
         UserInfo changeUser = (UserInfo) session.getAttribute("user");
         try
         {
-            if(!changeUser.getLoginPassword().equals(originPassword))
+            if(!originPassword.equals(decryptBasedDes(changeUser.getLoginPassword())))
             {
                 response.getWriter().write("<script> alert('原密码不正确'); location.href='/user/userinfo';</script>");
                 return;
@@ -206,8 +208,9 @@ public class UserController {
             }
             else
             {
-                changeUser.setLoginPassword(newPassword);
+                changeUser.setLoginPassword(encryptBasedDes(newPassword));
                 userService.updateUserInfo(changeUser);
+                session.setAttribute("user",changeUser);
                 response.getWriter().write("<script> alert('密码修改成功'); location.href='/user/userinfo';</script>");
             }
         }
@@ -215,6 +218,54 @@ public class UserController {
         {
             e.printStackTrace();
         }
+    }
+
+    /**
+     *  EDS的加密解密代码
+     */
+    private static final byte[] DES_KEY = { 21, 1, -110, 82, -32, -85, -128, -65 };
+    @SuppressWarnings("restriction")
+    public static String encryptBasedDes(String data) {
+        String encryptedData = null;
+        try {
+            // DES算法要求有一个可信任的随机数源
+            SecureRandom sr = new SecureRandom();
+            DESKeySpec deskey = new DESKeySpec(DES_KEY);
+            // 创建一个密匙工厂，然后用它把DESKeySpec转换成一个SecretKey对象
+            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+            SecretKey key = keyFactory.generateSecret(deskey);
+            // 加密对象
+            Cipher cipher = Cipher.getInstance("DES");
+            cipher.init(Cipher.ENCRYPT_MODE, key, sr);
+            // 加密，并把字节数组编码成字符串
+            Encoder encoder = Base64.getEncoder();
+            encryptedData = encoder.encodeToString(cipher.doFinal(data.getBytes()));
+        } catch (Exception e) {
+            // log.error("加密错误，错误信息：", e);
+            throw new RuntimeException("加密错误，错误信息：", e);
+        }
+        return encryptedData;
+    }
+    @SuppressWarnings("restriction")
+    public static String decryptBasedDes(String cryptData) {
+        String decryptedData = null;
+        try {
+            // DES算法要求有一个可信任的随机数源
+            SecureRandom sr = new SecureRandom();
+            DESKeySpec deskey = new DESKeySpec(DES_KEY);
+            // 创建一个密匙工厂，然后用它把DESKeySpec转换成一个SecretKey对象
+            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+            SecretKey key = keyFactory.generateSecret(deskey);
+            // 解密对象
+            Cipher cipher = Cipher.getInstance("DES");
+            cipher.init(Cipher.DECRYPT_MODE, key, sr);
+            // 把字符串进行解码，解码为为字节数组，并解密
+            Decoder decoder = Base64.getDecoder();
+            decryptedData = new String(cipher.doFinal(decoder.decode(cryptData)));
+        } catch (Exception e) {
+            throw new RuntimeException("解密错误，错误信息：", e);
+        }
+        return decryptedData;
     }
 }
 
